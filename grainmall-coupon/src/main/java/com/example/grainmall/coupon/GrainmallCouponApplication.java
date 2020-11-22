fb1421cc70b181d0b564d433ea9a2082c4e1a259
starwish <hn_ybs@163.com>
2020-11-22 21:20:02 +0800

    项目初始化

---
+++ grainmall-coupon/src/main/java/com/example/grainmall/coupon/GrainmallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.example.grainmall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainmallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrainmallCouponApplication.class, args);
+    }
+
+}
---
+++ grainmall-coupon/src/test/java/com/example/grainmall/coupon/GrainmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grainmall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrainmallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
